f0953ee7b4ef1ec89d6d369a5887b33eaa124c4b
CalmFarm <vhzpxj@gmail.com>
2024-12-28 21:25:53 +0900

    player and enemy characters

---
+++ Source/Aura/Private/Character/AuraMyCharacterBase.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/AuraMyCharacterBase.h"
+
+
+AAuraMyCharacterBase::AAuraMyCharacterBase()
+{
+ 	
+	PrimaryActorTick.bCanEverTick =	false;
+
+}
+
+
+void AAuraMyCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraMyCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraMyCharacterBase
+{
+	GENERATED_BODY()
+	
+};

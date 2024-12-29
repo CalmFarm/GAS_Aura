92d9ea4294de6d52a8dea58ef5c05b99ed6af5c1
CalmFarm <vhzpxj@gmail.com>
2024-12-29 16:01:10 +0900

    character blueprint setup

--- Source/Aura/Private/Character/AuraMyCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraMyCharacterBase.cpp
@@ -6,9 +6,12 @@
 
 AAuraMyCharacterBase::AAuraMyCharacterBase()
 {
- 	
 	PrimaryActorTick.bCanEverTick =	false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 }
 
 

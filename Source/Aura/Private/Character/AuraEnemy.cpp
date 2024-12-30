2979663cb836e302733b8ac46737c86bfbb841b9
CalmFarm <vhzpxj@gmail.com>
2024-12-30 19:44:45 +0900

    post procees highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,32 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	// 적의 메쉬가 Visibility 채널에 대해 충돌 응답을 블록으로 설정
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
+
 void AAuraEnemy::HighlightActor()
 {
-	bHighlighted = true;
+	// 적의 메쉬에 커스텀 깊이 렌더링 활성화
+	GetMesh()->SetRenderCustomDepth(true);
+	// 커스텀 깊이 스텐실 값을 빨간색으로 설정
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+
+	// 무기에 대한 커스텀 깊이 렌더링 활성화
+	Weapon->SetRenderCustomDepth(true);
+	// 무기의 커스텀 깊이 스텐실 값을 빨간색으로 설정
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bHighlighted = false;
+	// 적의 메쉬에 대한 커스텀 깊이 렌더링 비활성화
+	GetMesh()->SetRenderCustomDepth(false);
+    
+	// 무기에 대한 커스텀 깊이 렌더링 비활성화
+	Weapon->SetRenderCustomDepth(false);
 }

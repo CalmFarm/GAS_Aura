4d2ec0f117ddd32bcfdc9800160b18bbaf61287a
CalmFarm <vhzpxj@gmail.com>
2024-12-31 19:47:10 +0900

    Replicated Mode

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -14,8 +14,14 @@ AAuraEnemy::AAuraEnemy()
 
 	// AbilitySystemComponent를 UAuraAbilitySystemComponent 타입으로 기본 서브 오브젝트로 생성
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	
 	// AbilitySystemComponent의 복제 여부를 설정 (네트워크에서 복제 가능)
 	AbilitySystemComponent->SetIsReplicated(true);
+	
+	// 능력 시스템 컴포넌트의 복제 모드를 최소 모드로 설정합니다.
+	// Minimal 모드는 필요한 최소한의 데이터만 클라이언트에 전송하여,
+	// 네트워크 대역폭을 절약합니다.
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
 
 	// AttributeSet을 UAuraAttributeSet 타입으로 기본 서브 오브젝트로 생성
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -10,8 +10,14 @@ AAuraPlayerState::AAuraPlayerState()
 {
 	// AbilitySystemComponent를 UAuraAbilitySystemComponent 타입으로 기본 서브 오브젝트로 생성
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	
 	// AbilitySystemComponent의 복제 여부를 설정 (네트워크에서 복제 가능)
 	AbilitySystemComponent->SetIsReplicated(true);
+	
+	// 능력 시스템 컴포넌트의 복제 모드를 혼합 모드로 설정합니다.
+	// Mixed 모드는 서버와 클라이언트 간의 상태를 부분적으로 동기화하여,
+	// 필요한 경우에만 정보를 전송합니다.
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 
 	// AttributeSet을 UAuraAttributeSet 타입으로 기본 서브 오브젝트로 생성
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");

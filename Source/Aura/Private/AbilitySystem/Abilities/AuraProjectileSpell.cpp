f32f094c72595a2edf9a95a780249841725b2929
CalmFarm <vhzpxj@gmail.com>
2025-01-17 19:11:13 +0900

    Aura Projectile Spell

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -3,3 +3,14 @@
 
 #include "AbilitySystem/Abilities/AuraProjectileSpell.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("Active Ability"), true, true, FColor::Red, 3);
+	
+}
